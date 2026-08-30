247c59e5117440cc2fb3ab542e5d26600b005221
Yves Orton <demerphq@gmail.com>
2026-08-31 00:08:11 +0200

    Fix unparenthesized generator yields

--- perly.h
+++ perly.h
@@ -253,6 +253,6 @@ int yyparse (void);
 
 
 /* Generated from:
- * 8f1002d32c98de5b715910d2ba502aedf7c81027bdca93743a6a330a490a9b8f perly.y
+ * 10bb14e0e458c19d4e8ba2cf179c4d5fcab910a4406be8f63795a2582bcc9fa1 perly.y
  * 1a4cfc5b32006b09aba17cc80cd31ade3503ab1e829681adfe337d9c26e19fe0 regen_perly.pl
  * ex: set ro ft=c: */
